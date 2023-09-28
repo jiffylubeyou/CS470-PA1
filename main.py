0f9462c9a6e199b2ae40929b2a89df770135e06d
jiffylubeyou <59983470+jiffylubeyou@users.noreply.github.com>
2023-09-27 22:32:37 -0600

    got rid of silly things

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import random
 
 def coinToss():
@@ -137,7 +133,3 @@ if __name__ == '__main__':
     risk()
 
 
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
